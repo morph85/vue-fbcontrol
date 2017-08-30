500ee202cfaa90430cd6f71898e0f4d833eec5f1
morph85 <mononoctorus@gmail.com>
2017-08-30 19:32:21 +0800

    Updated to resolve dependency issue for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   //entry
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   //output
   output: {
     library: 'vue-fbcontrol',
